5b765102a3303a20b0835b6a28ee9c99ecca0bd5
og128 <gautheron.go@gmail.com>
2024-08-20 18:30:44 +0700

    oui

--- src/components/Logements/index.jsx
+++ src/components/Logements/index.jsx
@@ -22,7 +22,7 @@ function Logements(props) {
                     </div>
                 </div>
                 <div className='section-right'>
-                <div className='container-cons'>
+                    <div className='container-cons'>
                         {props.logement.cons.map((tag, idx) => {
                             return (
                                 <Cons cons={tag} key={idx} />
@@ -31,12 +31,30 @@ function Logements(props) {
                     </div>
                 </div>
             </section>
+            <section className='transportation'>
+                <div className='departure'>
+                    <h4>Departure</h4>
+                    <div className='d-data'>{props.logement.departure[0]}</div>
+                    {props.logement.departure[1] ?
+                        (
+                            <div className='tern'>or</div>
+                        ) : null}
+                    <div className='d-data'>{props.logement.departure[1]}</div>
+                </div>
+                <div className='leaving'>
+                    <h4>Leaving</h4>
+                    <div className="l-data">{props.logement.leaving[0]}</div>
+                    <div className="tern">and</div>
+                    <div className="l-data">{props.logement.leaving[1]}</div>
+                </div>
+
+            </section>
             <div className='container-dropdowns'>
                 <DropDown
                     title="Description"
                     text={props.logement.description} />
                 <DropDown
-                    title="Transportation"
+                    title="What to do"
                     text={<ul className='container-equipement'>{props.logement.equipments.map((item, idx) => {
                         return <li className='list-equipement' key={idx}>{item}</li>
                     })}</ul>} />
